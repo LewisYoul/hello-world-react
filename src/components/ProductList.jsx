68512b980876688125360936005f39b289db0b88
Lewis <lewisyoul@gmail.com>
2018-01-24 16:55:04 +0000

    Add propTypes to the ProductList component

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ProductList = (props) => {
   return(
     <div>ProductList</div>
   )
+}
+
+ProductList.propTypes = {
+  products: PropTypes.array.isRequired
 }
 
 export default ProductList;
