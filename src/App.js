6c78f2db37ecb38a6d00f7b0f1cfd70ad28caf4f
Lewis <lewisyoul@gmail.com>
2018-01-24 17:25:02 +0000

    Set products in State.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import ProductList from './components/ProductList'
 import './App.css';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [
+        {id: 1, name: 'AirMax 90', brand: 'Nike'},
+        {id: 2, name: 'Stan Smith', brand: 'Adidas'},
+        {id: 3, name: 'Classic', brand: 'Reebok'},
+      ]
+    }
+  }
 
   render() {
     return(
